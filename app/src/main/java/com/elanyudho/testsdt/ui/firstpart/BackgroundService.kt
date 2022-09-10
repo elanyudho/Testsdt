f032fa96632e7dceb2182fd0dff938212b686b39
elanyudho <elanyudho1@gmail.com>
2022-09-10 14:55:54 +0700

    button service

--- app/src/main/java/com/elanyudho/testsdt/ui/firstpart/BackgroundService.kt
+++ app/src/main/java/com/elanyudho/testsdt/ui/firstpart/BackgroundService.kt
@@ -2,23 +2,61 @@ package com.elanyudho.testsdt.ui.firstpart
 
 import android.app.Service
 import android.content.Intent
+import android.graphics.PixelFormat
 import android.os.IBinder
 import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.WindowManager
+import android.widget.Button
 import android.widget.Toast
+import com.elanyudho.testsdt.R
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.*
+
+
 @AndroidEntryPoint
 class BackgroundService : Service() {
 
     companion object {
         internal val TAG = BackgroundService::class.java.simpleName
     }
+    private lateinit var onButtonClickCallback: OnButtonClickCallback
+
+    fun setOnButtonClickCallback(onButtonClickCallback: OnButtonClickCallback) {
+        this.onButtonClickCallback = onButtonClickCallback
+    }
+
+    private val mLayoutParamsFlags = WindowManager.LayoutParams.FLAG_WATCH_OUTSIDE_TOUCH
+    // Views
+    private lateinit var mButtonView: View
+    private lateinit var mButton: Button
 
     private val serviceJob = Job()
     private val serviceScope = CoroutineScope(Dispatchers.Main + serviceJob)
 
     override fun onBind(intent: Intent): IBinder {
         throw UnsupportedOperationException("Not yet implemented")
+    }
+
+    override fun onCreate() {
+        val layoutInflater = getSystemService(LAYOUT_INFLATER_SERVICE) as LayoutInflater
+        mButtonView = layoutInflater.inflate(R.layout.layout_window_manager, null)
+
+        val params = WindowManager.LayoutParams(
+            WindowManager.LayoutParams.WRAP_CONTENT,
+            WindowManager.LayoutParams.WRAP_CONTENT,
+            WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY,
+            mLayoutParamsFlags,
+            PixelFormat.TRANSLUCENT
+        )
+
+        mButton = mButtonView.findViewById(R.id.btn_send_string) as Button
+        mButton.setOnClickListener { onButtonClickCallback.onItemClicked("HELLO WORLD!") }
+
+        val windowManager = getSystemService(WINDOW_SERVICE) as WindowManager
+        windowManager.addView(mButtonView, params)
+
     }
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
@@ -37,6 +75,13 @@ class BackgroundService : Service() {
     override fun onDestroy() {
         super.onDestroy()
         serviceJob.cancel()
+        val wm = getSystemService(WINDOW_SERVICE) as WindowManager
+        wm.removeView(mButtonView)
         Log.d(TAG, "onDestroy: Service Stopped")
     }
+
+    interface OnButtonClickCallback {
+        fun onItemClicked(data: String)
+    }
+
 }
--- app/src/main/java/com/elanyudho/testsdt/ui/firstpart/FirstPartActivity.kt
+++ app/src/main/java/com/elanyudho/testsdt/ui/firstpart/FirstPartActivity.kt
@@ -5,12 +5,15 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.elanyudho.testsdt.R
 import com.elanyudho.testsdt.databinding.ActivityFirstPartBinding
+import com.elanyudho.testsdt.utils.extension.visible
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class FirstPartActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityFirstPartBinding
+
+    private val bgService= BackgroundService()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,5 +23,13 @@ class FirstPartActivity : AppCompatActivity() {
         binding.btnServiceOn.setOnClickListener {
             startService(Intent(this@FirstPartActivity, BackgroundService::class.java))
         }
+
+        bgService.setOnButtonClickCallback(object :BackgroundService.OnButtonClickCallback {
+            override fun onItemClicked(data: String) {
+                binding.tvMessage.text = data
+                binding.tvMessage.visible()
+            }
+
+        })
     }
 }
